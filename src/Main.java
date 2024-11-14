20e60d6abf9dbbf4ed723b1d71d4cff15d33bf5c
agorohov <aleksandr.gorohov@gmail.com>
2024-11-14 10:13:51 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(5);
+
+        // потоки-производители
+        for (int i = 0; i < 3; i++) {
+            new Thread(() -> {
+                try {
+                    for (int j = 0; j < 10; j++) {
+                        simpleBlockingQueue.enqueue(j);
+                        System.out.println("В очередь добавлено: " + j);
+                        Thread.sleep((long) (Math.random() * 1000));
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }).start();
+        }
+
+        // потоки-потребители
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                try {
+                    for (int j = 0; j < 15; j++) {
+                        Integer item = simpleBlockingQueue.dequeue();
+                        System.out.println("Из очереди получено: " + item);
+                        Thread.sleep((long) (Math.random() * 1000));
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }).start();
+        }
     }
 }
--- src/SimpleBlockingQueue.java
+++ src/SimpleBlockingQueue.java
@@ -1,2 +1,45 @@
-public class SimpleBlockingQueue {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SimpleBlockingQueue<T> {
+
+    private final Queue<T> queue = new LinkedList<>();
+
+    private final int capacity;
+    private static final int DEFAULT_CAPACITY = 10;
+    private int count = 0;
+
+    public SimpleBlockingQueue(int capacity) {
+        if (capacity <= 0) throw new IllegalArgumentException();
+        this.capacity = capacity;
+    }
+
+    public SimpleBlockingQueue() {
+        this.capacity = DEFAULT_CAPACITY;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    // добавление элемента в очередь
+    public synchronized void enqueue(T item) throws InterruptedException {
+        while (count == capacity) {
+            wait();
+        }
+        queue.add(item);
+        count++;
+        notifyAll();
+    }
+
+    // извлечение элемента из очереди
+    public synchronized T dequeue() throws InterruptedException {
+        while (count == 0) {
+            wait();
+        }
+        T item = queue.poll();
+        count--;
+        notifyAll();
+        return item;
+    }
 }
